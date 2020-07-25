7b968e7ba46c034b2c6355ff6aee2f93113b8f65
Ole Hagberg <ole.kh@hotmail.com>
2020-07-26 01:57:35 +0200

    GIT IGNORE

--- config.cs
+++ config.cs
@@ -2,7 +2,7 @@ public class config
 {
     public static string token()
     {
-        return "NzE1MTYyOTUyNjA4Nzc2MTky.XxIbNQ.FGV_3JTGzwO6pOaaHzW0_zo0yEk";
+        return "";
     }
     public static string prefix()
     {
